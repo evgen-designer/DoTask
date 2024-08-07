bf4cdfddf0b4ca46637ae08556f3b67eb6063926
Ievgen Kuznietsov <ievgen.contact@gmail.com>
2024-08-07 18:36:17 +0100

    Initial Commit

---
+++ DoTask/DoTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoTaskApp.swift
+//  DoTask
+//
+//  Created by Mac on 07/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct DoTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
